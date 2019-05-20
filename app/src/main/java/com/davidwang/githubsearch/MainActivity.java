34192b58150f19860e87faa5360731a8bf784654
daviwang <daviwang@umich.edu>
2019-05-19 22:40:40 -0400

    Began working on RecyclerView for MainActivity

--- app/src/main/java/com/davidwang/githubsearch/MainActivity.java
+++ app/src/main/java/com/davidwang/githubsearch/MainActivity.java
@@ -2,17 +2,33 @@ package com.davidwang.githubsearch;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.content.Intent;
+import android.support.v7.widget.LinearLayoutManager;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);     // for better performance
+
+        // Use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // Specify the adapter
+        mAdapter = new MyAdapter(myDataset);
+        recyclerView.setAdapter(mAdapter);
     }
 
     // Populates overflow menu with items
